b96234607dd671630d11fa60ec827184f74561b4
kéven cyr <1427141@cstj.qc.ca>
2017-11-09 12:15:48 -0500

    Création du projet.

---
+++ CYRK1427141_tp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CYRK1427141_tp2
+//
+//  Created by Keven Cyr on 17-11-09.
+//  Copyright © 2017 Keven Cyr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
